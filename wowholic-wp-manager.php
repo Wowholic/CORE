41bbdf24731efe5a92acfbb9380644fe871a9e01
Bruno De Angelis <brunordeangelis@gmail.com>
2021-08-05 15:08:50 -0300

    init plugin

---
+++ wowholic-wp-manager.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Wowholic WP Manager
+* Plugin URI: https://wowholic.com
+* Description: Wowholic plugin for managing common tasks of WordPress
+* Version: 0.1.0
+* Author: Wowholic
+* Author URI: https://wowholic.com
+**/
