0884b2a78330f78d990fe791795bd7cd7c9395cc
Bruno De Angelis <brunordeangelis@gmail.com>
2021-11-16 11:32:40 -0300

    refactor: push vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '04b97a727ff7f304bb6c86112d9d954f436992ea',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '04b97a727ff7f304bb6c86112d9d954f436992ea',
+            'dev_requirement' => false,
+        ),
+        'htmlburger/carbon-fields' => array(
+            'pretty_version' => 'v3.3.2',
+            'version' => '3.3.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../htmlburger/carbon-fields',
+            'aliases' => array(),
+            'reference' => 'dd5663e14c6db365323b688dbae1cfbeaf14bee7',
+            'dev_requirement' => false,
+        ),
+    ),
+);
